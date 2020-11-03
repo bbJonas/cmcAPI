23b7098a40c954b60e046ada17601d3fd8cdab9c
bbJonas <43041467+bbJonas@users.noreply.github.com>
2020-11-03 18:07:36 +0100

    request working

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 /* Example in Node.js ES6 using request-promise */
-
+const request = require('request')
 const rp = require('request-promise');
+
 const requestOptions = {
   method: 'GET',
-  uri: 'https://undefined/v1/cryptocurrency/listings/latest',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
   qs: {
     'start': '1',
-    'limit': '5000',
-    'convert': 'USD'
+    'limit': '50',
+    'convert': 'EUR'
   },
   headers: {
-    'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
+    'X-CMC_PRO_API_KEY': '1e6724c7-1939-42eb-aa2c-7c5903a58e8e'
   },
   json: true,
   gzip: true
 };
 
 rp(requestOptions).then(response => {
-  console.log('API call response:', response);
+    //console.log('API call response:', response);
+    
+    var addMarketCapRank = function(rawData){
+        rawData.data.forEach((coin) => {
+            console.log(coin.name);
+            console.log('Price (USD):');
+            console.log(coin.quote.EUR.price);
+            console.log('MarketCap:');
+            console.log(coin.quote.EUR.volume_24h)
+            console.log('   ')
+        })
+    };
+    
+addMarketCapRank(response);
+    
 }).catch((err) => {
   console.log('API call error:', err.message);
 });
